6cf152d08c463331104bbc190cac09808fb2f6ae
Jeffe_Dahmer <teermadeline@gmail.com>
2026-01-21 19:57:03 -0600

    Adding Avionics Code

---
+++ Avionics/AvionicsSoftware/FullCode/FullCode.ino
@@ -0,0 +1,18 @@
+// Elysium Avionics Software
+
+
+void sensorSample();
+void filter();
+void sendTelemetry();
+void calcAltitude();
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
