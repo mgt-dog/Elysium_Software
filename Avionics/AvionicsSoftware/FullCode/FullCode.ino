3c979ad905727e22c203e96559952a117628c414
WVpilot <59519152+WVpilot@users.noreply.github.com>
2026-01-29 01:44:06 -0600

    Untested RK4 code
    I think that I finished the general RK4 stuff somewhat. Gonna test for accuracy later this week once I get the time. If inaccurate there are some changes I can make like adding trapezoidal integration and making density a function of altitude.

--- Avionics/AvionicsSoftware/FullCode/FullCode.ino
+++ Avionics/AvionicsSoftware/FullCode/FullCode.ino
@@ -10,10 +10,11 @@ void sensorSample();
 void filter();
 void sendTelemetry();
 float calcApogee();
-float calcVelocity();
+float calcVelocity(float h, float v_x, float v_y, float p, float area, float C_d);
 float  calcAcceleration(float velocity_x, float velocity_y, float p, float area);
 
 //Global Variables
+  float altitude;
   float velocityX, velocityY, velocityZ;
   float accelerationX, accelerationY, accelerationZ;
   float temperature, pressure;
@@ -83,8 +84,6 @@ void sensorSample(){
 
 }
 
-float
-
 float calcApogee() {
   // calculate the apogee based on current velocity, altitude, and elevation angle
   // based on the fourth order  runge-kutta method for approximation
@@ -92,22 +91,34 @@ float calcApogee() {
   const float t_h = 0.1;
   const float m = 22.389; //Kilograms
   const float A_r = 0.019; //m^2
-  float y_p = CURRENT_ALTITUDE;
-  float v_x = (CURRENT_VELOCITY.X^2+CURRENT_VELOCITY.Y^2)^(1/2);
-  float v_y = CURRENT_VELOCITY.Z;
-  float p; //kg/m^3
+  const float C_d = 0.375585;
+  float y_p = altitude;
+  float v_x = pow(pow(velocityX,2)+pow(velocityY,2),1/2);
+  float v_y = velocityZ;
+  float p = 1.225; //kg/m^3
   float apogee;
-  while (v_y > 0);
-
-float calcVelocity() {
-
+  float k_1;
+  float k_2;
+  float k_3;
+  float k_4;
+  while (v_y > 0) {
+    k_1 = calcVelocity(t_h, v_x, v_y, p, A_r, C_d);
+    k_2 = calcVelocity(t_h/2, v_x, v_y, p, A_r, C_d);
+    k_3 = calcVelocity(t_h/2, v_x, v_y, p, A_r, C_d);
+    k_4 = calcVelocity(t_h, v_x, v_y, p, A_r, C_d);
+    y_p = y_p + t_h/6*(k_1+2*k_2+2*k_3+k_4);
+  }
+  apogee = y_p;
+  Serial.printf("Predicted apogee is %f", apogee);
+  return apogee;
 }
 
-float calcAcceleration(float velocity[], float p, float area, float C_d) {
-  float a;
-  float theta = atan(velocity[1]/velocity[0]);
-  float A_d = sin(theta)*0.5*p*C_d*(velocity[0]^2+velocity[1]^2);
-
-
-  return apogee;
+float calcVelocity(float h, float v_x, float v_y, float p, float area, float C_d) {
+  float theta = atan(v_y/v_x);
+  float a_d = -1*sin(theta)*0.5*p*C_d*(pow(v_x,2)+pow(v_y,2));
+  float a_g = -1*9.80665;
+  float a = a_d + a_g;
+  v_y -= a*h;
+  
+  return 0;
 }
