c0ef547c7643981156b1b4fc4e627f38805269df
jaa0053-sketch <jaa0053@uah.edu>
2026-01-28 19:15:02 -0600

    Create Pin_numbas.ino
    pin numbas

---
+++ Pin_numbas.ino
@@ -0,0 +1,17 @@
+
+#define SDA 4 
+#define SCL 5
+#define TX 43
+#define RX 44
+#define SCK3 39
+#define D03 41
+#define DI3 40
+#define CS3 42
+#define CS3_S 38
+#define STEP 37
+#define DIR 36
+#define CS2 16
+#define D02 17
+#define SCK2 18
+#define DI2 19
+
